bcf0ae983b2ee28a24d23d2a4ef688707bb84623
baf62495 <baf62495@aol.com>
2020-03-09 22:51:50 -0400

    basic landing page static

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './LandingPage';
 
 export default function LandingPage(props) {
   return (
     <div className='landing-page'>
+      <h1>Handle Leads With Stream</h1>
       <button onClick={e => props.history.push('/pipelines')}>Enter</button>
     </div>
   );
